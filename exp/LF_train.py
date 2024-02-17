bb9d48a508ab154811ac47006880a069bcfccfac
Mark Mueller <mark.mueller@inf.ethz.ch>
2024-02-17 19:37:01 +0100

    Debugging Z3 bf16

--- exp/LF_train.py
+++ exp/LF_train.py
@@ -31,74 +31,16 @@ _TRAIN_ARGS = [ModelArguments, DataArguments, Seq2SeqTrainingArguments, Finetuni
 def run_exp(args: Optional[Dict[str, Any]] = None, callbacks: Optional[List["TrainerCallback"]] = None):
     model_args, data_args, training_args, finetuning_args, generating_args = _parse_train_args(args)
 
+    model, tokenizer = load_model_and_tokenizer()
+
+    _verify_model_args(model_args, finetuning_args)
+
     # Setup logging
     if training_args.should_log:
         _set_transformers_logging()
 
-    _verify_model_args(model_args, finetuning_args)
-
-    model, tokenizer = load_model_and_tokenizer()
-
-
-    # postprocess training_args
-    if (
-        training_args.local_rank != -1
-        and training_args.ddp_find_unused_parameters is None
-        and finetuning_args.finetuning_type == "lora"
-    ):
-        print("Ping 1")
-        logger.warning("`ddp_find_unused_parameters` needs to be set as False for LoRA in DDP training.")
-        training_args_dict = training_args.to_dict()
-        training_args_dict.update(dict(ddp_find_unused_parameters=False))
-        training_args = Seq2SeqTrainingArguments(**training_args_dict)
-
-    can_resume_from_checkpoint = True
-
-    if (
-        training_args.resume_from_checkpoint is None
-        and training_args.do_train
-        and os.path.isdir(training_args.output_dir)
-        and not training_args.overwrite_output_dir
-        and can_resume_from_checkpoint
-    ):
-        print("Ping 2")
-        last_checkpoint = get_last_checkpoint(training_args.output_dir)
-        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-            raise ValueError("Output directory already exists and is not empty. Please set `overwrite_output_dir`.")
-
-        if last_checkpoint is not None:
-            training_args_dict = training_args.to_dict()
-            training_args_dict.update(dict(resume_from_checkpoint=last_checkpoint))
-            training_args = Seq2SeqTrainingArguments(**training_args_dict)
-            logger.info(
-                "Resuming training from {}. Change `output_dir` or use `overwrite_output_dir` to avoid.".format(
-                    training_args.resume_from_checkpoint
-                )
-            )
-
-    # postprocess model_args
-    model_args.compute_dtype = (
-        torch.bfloat16 if training_args.bf16 else (torch.float16 if training_args.fp16 else None)
-    )
-    model_args.model_max_length = data_args.cutoff_len
-
-    # Log on each process the small summary:
-    logger.info(
-        "Process rank: {}, device: {}, n_gpu: {}\n  distributed training: {}, compute dtype: {}".format(
-            training_args.local_rank,
-            training_args.device,
-            training_args.n_gpu,
-            bool(training_args.local_rank != -1),
-            str(model_args.compute_dtype),
-        )
-    )
-    logger.info(f"Training/evaluation parameters {training_args}")
-
-
     # Set seed before initializing model.
     set_seed(training_args.seed)
-
-    # model_args, data_args, training_args, finetuning_args, generating_args = get_train_args(args)
 
     callbacks = [LogCallback()] if callbacks is None else callbacks
 
